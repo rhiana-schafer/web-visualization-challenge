3dadf2bef6955a66099b735cbba6c714151e4d0b
rhiana-schafer <66378414+rhiana-schafer@users.noreply.github.com>
2022-06-17 16:04:40 -0500

    all initial visualizations created, just need to fix dropdown updating

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,47 +1,68 @@
 //Use the D3 library to read in samples.json from the URL
-const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-data = d3.json(samples)
+const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-//test that import worked
-data.then(function(data)
-    {console.log(data.samples[0].sample_values.slice(0,10));});
+d3.json(samples).then(function(data) {
+    //create visualizations based on first sample
+    bar(data.samples[0]);
+    bubble(data.samples[0]);
+    metadata(data.metadata[0]);
+    //create dropdown
+    let dropDown = d3.selectAll("#selDataset");
+    let dropdown_options = data.names;
+    for (let i = 0; i < dropdown_options.length; i++) {
+        dropDown.append("option").text(dropdown_options[i])
+      };
+    //update charts based on dropdown selections
+    dropDown.on("change", optionChanged);
+  });
 
+//update with dropdown selection -- this doesnt work yet!!
+function optionChanged(sample_id) {
+    console.log(sample_id);
+    let samples = data.samples;
+    let meta_info = data.metadata;
+    for (let i = 0; i < samples.length; i++) {
+        if (sample.id === sample_id) {
+            bar(samples[i]);
+            bubble(samples[i])
+            metadata(meta_info[i])
+        };
+    };
+};
 
-//create initial bar chart based on sample 0
-function init() {
-    let sample = data.samples[0]
-    let values = sample.sample_values.slice(0,10)
-    let labels = sample.otu_ids.slice(0,10)
-    let hovertext = sample.otu_labels.slice(0,10)
-  
-    
-    let data = [{
-        values: values,
-        labels: labels,
-        text: hovertext,
-        type: "bar"
-    }]
+function bar(sample) {
+    let bardata = [{
+        type: 'bar',
+        x: sample.sample_values.slice(0,10).reverse(),
+        y: sample.otu_ids.slice(0,10).map(id=>"OTU"+id).reverse(),
+        hovertext: sample.otu_labels.slice(0,10).reverse(),
+        orientation: 'h'
+    }];
+    Plotly.newPlot("bar", bardata);
+};
+
+function bubble(sample){
+    let trace1 = {
+        x: sample.otu_ids,
+        y: sample.sample_values,
+        text: sample.otu_labels,
+        mode: 'markers',
+        marker: {
+          size: sample.sample_values,
+          color: sample.otu_ids}
+    };
+    let data = [trace1];
     let layout = {
         height: 600,
-        width: 800
+        width: 1000,
+        xaxis: {title: {text: 'OTU'}},
     };
-    Plotly.newPlot("bar", data, layout)};
-      
-//Create a horizontal bar chart with a dropdown menu to display 
-//the top 10 OTUs found in that individual.
-    //Use sample_values as the values for the bar chart.
-    //Use otu_ids as the labels for the bar chart.
-    //Use otu_labels as the hovertext for the chart.
+    Plotly.newPlot('bubble', data, layout);    
+};
 
-//Create a bubble chart that displays each sample.
-    //Use otu_ids for the x values.
-    //Use sample_values for the y values.
-    //Use sample_values for the marker size.
-    //Use otu_ids for the marker colors.
-    //Use otu_labels for the text values.
-
-//Display the sample metadata, i.e., an individual's demographic information.
-
-//Display each key-value pair from the metadata JSON object somewhere on the page.
-
-//Update all the plots when a new sample is selected. 
+function metadata(sample){
+    let meta_info = d3.selectAll(".panel-body");
+    meta_info.html(`id: ${sample.id} <br> ethnicity: ${sample.ethnicity}<br> gender: 
+        ${sample.gender} <br> age: ${sample.age}<br> location: ${sample.location}<br> bbtype: 
+        ${sample.bbtype}<br> wfreq: ${sample.wfreq}`);
+};
